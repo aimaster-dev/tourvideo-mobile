f303c4dffe436efce43a362db0c3a98dad401558
Komal Parulekar <Komalp.aobs@gmail.com>
2026-02-23 19:57:14 +0530

    android ffmpeg fixes

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,4 @@
-//metro.config.js
+// metro.config.js
 const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 
 const defaultConfig = getDefaultConfig(__dirname);
@@ -12,12 +12,29 @@ const { assetExts, sourceExts } = defaultConfig.resolver;
  */
 const config = {
   transformer: {
-    babelTransformerPath: require.resolve("react-native-svg-transformer")
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
   },
   resolver: {
     assetExts: assetExts.filter((ext) => ext !== "svg"),
-    sourceExts: [...sourceExts, "svg"]
-  }
+    sourceExts: [...sourceExts, "svg"],
+
+    // --- Axios browser build fix ---
+    resolveRequest: (context, moduleName, platform) => {
+      if (moduleName === "axios" || moduleName.startsWith("axios/")) {
+        return context.resolveRequest(
+          {
+            ...context,
+            unstable_conditionNames: ["browser"], // forces browser build
+          },
+          moduleName,
+          platform
+        );
+      }
+
+      // Default resolution for all other modules
+      return context.resolveRequest(context, moduleName, platform);
+    },
+  },
 };
 
 module.exports = mergeConfig(defaultConfig, config);
